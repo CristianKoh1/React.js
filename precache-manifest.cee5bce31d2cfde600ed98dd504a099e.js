1e487570e5d69f899d5a87c3ec293ace1314bfa4
cristian koh <cristianrodrigokohuc@gmail.com>
2020-06-04 08:33:46 -0500

    App reactjs

---
+++ precache-manifest.cee5bce31d2cfde600ed98dd504a099e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e67470aaee4dc2697a436a9b2cfa237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "63d39c699ee49f4ca20c",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "11ef6a64d9bf7d0edf08",
+    "url": "/static/css/main.4a7b0a38.chunk.css"
+  },
+  {
+    "revision": "63d39c699ee49f4ca20c",
+    "url": "/static/js/2.a3d561cc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a3d561cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ef6a64d9bf7d0edf08",
+    "url": "/static/js/main.c4c1a511.chunk.js"
+  },
+  {
+    "revision": "216563cf50e7c35b2f13",
+    "url": "/static/js/runtime-main.7c0ae09a.js"
+  },
+  {
+    "revision": "3cb5f20b8ae12193792f03dccf299755",
+    "url": "/static/media/fondo1.3cb5f20b.jpg"
+  }
+]);
